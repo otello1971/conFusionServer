8088b30bcc26d42fa491c9c8428ebf97e2c6a52d
Pablo Moraga <pablo.moraga.aedo@gmail.com>
2017-12-21 11:06:38 -0300

    Express REST API with MongoDB and Mongoose Part 1

--- app.js
+++ app.js
@@ -10,6 +10,32 @@ var users = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+// -------------------------------------------
+// --    MONGODB LIBRARY IMPORTING          --
+// -------------------------------------------
+const mongoose = require('mongoose');
+//mongoose.Promise = require('bluebird');
+// Use native promises
+mongoose.Promise = global.Promise;
+
+const Dishes = require('./models/dishes');
+
+// -------------------------------------------
+// --           MONGODB CONNECTION          --
+// -------------------------------------------
+//const url = 'mongodb://localhost:27017/conFusion';
+const url = 'mongodb://192.168.188.129:27017/conFusion';
+
+const connect = mongoose.connect(url, {
+  useMongoClient: true
+});
+
+connect.then((db) => {
+  console.log('Connected correctly to server');
+})
+  .catch((err) => {
+    console.log(err);
+  });
 
 var app = express();
 
