66c70dd962171a1848c04bbfac40d59d261f7a9a
Pablo Moraga <pablo.moraga.aedo@gmail.com>
2017-12-29 16:17:12 -0300

    Passport

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 // -------------------------------------------
 // --    MONGODB LIBRARY IMPORTING          --
 // -------------------------------------------
@@ -65,26 +68,23 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', index);
 app.use('/users', users);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');                          
+    err.status = 401;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
